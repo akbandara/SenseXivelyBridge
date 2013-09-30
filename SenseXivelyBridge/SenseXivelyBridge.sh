9922facb583a20c4cf0813b70e4bf76f06b63cc1
akbandara <a.k.bandara@open.ac.uk>
2013-09-30 15:53:34 +0100

    Added shell launcher and library for xively_python

---
+++ SenseXivelyBridge/SenseXivelyBridge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PYTHONPATH=./src/:/lib/xively_python-0.1.0_rc2-py3.3.egg:$PYTHONPATH
+export HTTP_PROXY=wwwcache.open.ac.uk:80
+export HTTPS_PROXY=wwwcache.open.ac.uk:80
+
+python3.3 -m OUSense.SenseXivelyBridge
