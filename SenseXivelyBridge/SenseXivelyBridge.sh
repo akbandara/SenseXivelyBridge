f68c2c0da4361a3fe3f9fecf8a7f6d90b04b4280
akbandara <a.k.bandara@open.ac.uk>
2013-10-22 15:41:52 +0100

    Fixed shell script to pass on command line args for server and port settings

--- SenseXivelyBridge/SenseXivelyBridge.sh
+++ SenseXivelyBridge/SenseXivelyBridge.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=./src/:/lib/xively_python-0.1.0_rc2-py3.3.egg:$PYTHONPATH
 export HTTP_PROXY=wwwcache.open.ac.uk:80
 export HTTPS_PROXY=wwwcache.open.ac.uk:80
 
-python3.3 -m OUSense.SenseXivelyBridge
+python3.3 -m OUSense.SenseXivelyBridge $1 $2 $3 $4
